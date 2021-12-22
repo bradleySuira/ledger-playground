3cf7bdde159e16075f36b837072354e31e6691ed
Bradley <bradleysuira@gmail.com>
2021-12-21 22:51:15 -0500

    removed unused imports

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,4 @@
 module.exports = {
-    transpileDependencies: [
-        // "@liquality/bitcoin-esplora-batch-api-provider",
-        // "@liquality/bitcoin-ledger-provider",
-        // "@liquality/bitcoin-rpc-fee-provider",
-        // "@liquality/client",
-        // "@liquality/types",
-        // "@liquality/bitcoin-networks",
-        // "@liquality/bitcoin-esplora-swap-find-provider",
-        // "@liquality/bitcoin-utils",
-        // "@ledgerhq/hw-transport-webhid",
-        // "bignumber.js"
-      ]
+  
 }
   
